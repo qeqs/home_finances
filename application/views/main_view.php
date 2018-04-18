30ac62395fcba9b8835ae70e4e14d6bb825a4dc2
Sashkalol <fairprot@mail.ru>
2018-04-19 00:38:18 +0300

    Delete footer Add logout Change Sign up -> Registration

--- application/views/main_view.php
+++ application/views/main_view.php
@@ -3,4 +3,4 @@
 <!--  <img src="/images/...jpg" align="left">-->
 	    <? session_start() ?>
   <a href="/">Home Finances</a> - Hello, <? echo $_SESSION['user']->name; ?></p>
-  <h2><a href="/login" id="loginform">Sign in</a> | <a href="registration" id="registerform">Sign up</a></h2>
+  <h2><a href="/login" id="loginform">Sign in</a> | <a href="registration" id="registerform">Registration</a></h2>
