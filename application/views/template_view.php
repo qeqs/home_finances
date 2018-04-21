db56ec710970aea60cc608e80ab055893cf7e012
Sashkalol <fairprot@mail.ru>
2018-04-21 13:41:52 +0300

    Delete footer Add logout Change Sign up -> Registration

--- application/views/template_view.php
+++ application/views/template_view.php
@@ -1,14 +1,9 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
         "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<!--
-Design by Free CSS Templates
-http://www.freecsstemplates.org
--->
+
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
-    <meta name="description" content=""/>
-    <meta name="keywords" content=""/>
     <title>Home Finances</title>
     <link href="http://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" type="text/css"/>
     <link href="http://fonts.googleapis.com/css?family=Kreon" rel="stylesheet" type="text/css"/>
@@ -33,15 +28,7 @@ http://www.freecsstemplates.org
             <br class="clearfix"/>
         </div>
     </div>
-    <div id="page">
-        <h3>Main menu</h3>
-        <ul>
-            <li class="first active"><a href="/">Main</a></li>
-            <li><a href="/finances">Finances</a></li>
-            <li><a href="/plans">Plans</a></li>
-            <li class="last"><a href="/charts">Charts</a></li>
-        </ul>
-    </div>
+
     <div id="content">
         <div class="box">
             <?php include 'application/views/' . $content_view; ?>
