965ead3403eaaea921d7379847b5b82567562c30
qeqs <vadimlygin@yandex.ru>
2018-04-21 22:19:43 +0300

    finance view fix

--- application/views/finances_view.php
+++ application/views/finances_view.php
@@ -2,13 +2,13 @@
 <p>
 <table>
     All finances of <? session_start(); echo $_SESSION['user']->name;?>
-    <tr><td>Date</td><td>Value</td><td>Description</td><td>Type<td>Name</td><td>Description</td></td></tr>
+    <tr><td>Date</td><td>Value</td><td>Description</td><td>Type</td></tr>
 <?php
 
 	foreach($data as $row)
     {
-        echo '<tr><td>'.$row['Date'].'</td><td>'.$row['Value'].'</td><td>'.$row['Description'].'</td><td><td>'.$row['Type']['Value'].'</td><td>'.$row['Type']['Description'].'</td></td></tr>';
+        echo '<tr><td>'.$row['Date'].'</td><td>'.$row['Value'].'</td><td>'.$row['Description'].'</td><td>'.$row['Type']['Value'].'</td></tr>';
     }
 ?>
-</table>
+
 </p>
