7a5814d1b62ac02b6a45d5f562ee6d2aa8a52d7d
qeqs <vadimlygin@yandex.ru>
2018-04-21 21:19:36 +0300

    fix

--- application/views/chart_view.php
+++ application/views/chart_view.php
@@ -3,29 +3,30 @@
     google.charts.load('current', {'packages': ['corechart']});
     google.charts.setOnLoadCallback(drawCharts);
 
-    function chart(chartData, div_class) {
-        var data = google.visualization.arrayToDataTable(chartData);
-        var options = {
-            title: 'Total Finances',
-            curveType: 'function',
-            legend: {position: 'bottom'}
-        };
-        var chart = new google.visualization.LineChart(document.getElementById(div_class));
-        chart.draw(data, options);
+    function chart(chartData, div_class, title) {
+        if (chartData.length > 0) {
+            var data = google.visualization.arrayToDataTable(chartData);
+            var options = {
+                title: title,
+                curveType: 'function',
+                legend: {position: 'bottom'}
+            };
+            var chart = new google.visualization.LineChart(document.getElementById(div_class));
+            chart.draw(data, options);
+        }
     }
 
     function drawCharts() {
         <?php if($data != null){?>
-        chart(<?php echo json_encode($data['IncomeOutcomeChart']) ?>, 'income_outcome');
-        chart(<?php echo json_encode($data['IncomeChart']) ?>, 'income');
-        chart(<?php echo json_encode($data['OutcomeChart']) ?>, 'outcome');
+        chart(<?php echo json_encode($data['IncomeOutcomeChart']) ?>, 'income_outcome', 'Total finances');
+        chart(<?php echo json_encode($data['IncomeChart']) ?>, 'income', 'Income finances');
+        chart(<?php echo json_encode($data['OutcomeChart']) ?>, 'outcome', 'Outcome finances');
         <?}
         else {
             echo "<p>There is nothing to show</p>";
         }?>
     }
 </script>
-<br/>
-    <div id="income_outcome" style="position: center; width: 900px; height: 500px"></div>
-    <div id="income" style="position: center; width: 900px; height: 500px"></div>
-    <div id="outcome" style="position: center; width: 900px; height: 500px"></div>
+<div id="income_outcome"></div>
+<div id="income"></div>
+<div id="outcome"></div>
