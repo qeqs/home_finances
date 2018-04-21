c3893356b3fa43c98ad4f3396e40d5d927b01999
Sashkalol <fairprot@mail.ru>
2018-04-21 23:20:49 +0300

    Валидация для регестрации и авторизации

--- application/views/login_view.php
+++ application/views/login_view.php
@@ -7,11 +7,11 @@
 	</tr>
 	<tr>
 		<td>login</td>
-		<td><input type="text" name="login"></td>
+		<td><input type="text" name="login" required></td>
 	</tr>
 	<tr>
 		<td>password</td>
-		<td><input type="password" name="password"></td>
+		<td><input type="password" name="password" required></td>
 	</tr>
 	<th colspan="2" style="text-align: right">
 	<input type="submit" value="Submit" name="btn"
--- application/views/registration_view.php
+++ application/views/registration_view.php
@@ -7,15 +7,15 @@
         </tr>
         <tr>
             <td>name</td>
-            <td><input type="text" name="username"></td>
+            <td><input type="text" name="username" required></td>
         </tr>
         <tr>
             <td>login</td>
-            <td><input type="text" name="login"></td>
+            <td><input type="text" name="login" required></td>
         </tr>
         <tr>
             <td>password</td>
-            <td><input type="password" name="password"></td>
+            <td><input type="password" name="password" required></td>
         </tr>
         <th colspan="2" style="text-align: right">
             <input type="submit" value="Submit" name="btn"
