b5f6df3e885fad6b11dc33696a99fcb50f185a1f
Sashkalol <fairprot@mail.ru>
2018-04-22 22:55:08 +0300

    Правки css от говна

--- application/views/finances_view.php
+++ application/views/finances_view.php
@@ -2,7 +2,7 @@
 <p>
 <table>
     All finances of <? session_start(); echo $_SESSION['user']->name;?>
-    <tr align = "center"><td>Date</td><td>Value</td><td>Description</td><td>Type</td><td></td></tr>
+    <tr><td>Date</td><td>Value</td><td>Description</td><td>Type</td><td></td></tr>
 <?php
 
 	foreach($data as $row)
@@ -10,7 +10,7 @@
         echo '<tr><td>'.$row['Date'].'</td><td>'.$row['Value'].'</td><td>'.$row['Description'].'</td><td>'.$row['Type']['Value'].'</td><td>Delete/Edit</td></tr>';
     }
     echo '<form id="add", action="/finances/add", method="post">
-            <tr align = "center"><td><input type="date", name="Date"></td>
+            <tr align = "left"><td><input type="date", name="Date"></td>
             <td><input type="number", name="Value"></td>
             <td><input type="text", name="Description"></td>
             <td><input type="text", name="Type"></td>
