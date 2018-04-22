42a9066df30ee6f49391a51ed477e6e3b54fd063
Sashkalol <fairprot@mail.ru>
2018-04-23 00:42:02 +0300

    Правки css от говна

--- application/views/finances_view.php
+++ application/views/finances_view.php
@@ -1,21 +1,31 @@
 <h1>Finances</h1>
 <p>
-<table>
-    All finances of <? session_start(); echo $_SESSION['user']->name;?>
-    <tr><td>Date</td><td>Value</td><td>Description</td><td>Type</td><td></td></tr>
-<?php
+<div id="test">
+    <div class="newbox">
+        <table>
+            All finances of <? session_start();
+            echo $_SESSION['user']->name; ?>
+            <tr>
+                <td>Date</td>
+                <td>Value</td>
+                <td>Description</td>
+                <td>Type</td>
+                <td></td>
+            </tr>
+            <?php
 
-	foreach($data as $row)
-    {
-        echo '<tr><td>'.$row['Date'].'</td><td>'.$row['Value'].'</td><td>'.$row['Description'].'</td><td>'.$row['Type']['Value'].'</td><td>Delete/Edit</td></tr>';
-    }
-    echo '<form id="add", action="/finances/add", method="post">
+            foreach ($data as $row) {
+                echo '<tr><td>' . $row['Date'] . '</td><td>' . $row['Value'] . '</td><td>' . $row['Description'] . '</td><td>' . $row['Type']['Value'] . '</td><td>Delete/Edit</td></tr>';
+            }
+            echo '<form id="add", action="/finances/add", method="post">
             <tr align = "left"><td><input type="date", name="Date"></td>
             <td><input type="number", name="Value"></td>
             <td><input type="text", name="Description"></td>
             <td><input type="text", name="Type"></td>
             <td><button form="add">Add</button></td></tr>
           </form>';
-?>
-</table>
+            ?>
+        </table>
+    </div>
+</div>
 </p>
