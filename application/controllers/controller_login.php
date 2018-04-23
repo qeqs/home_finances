8330aa8158881871f7f85194286bfb342a33a9a8
qeqs <vadimlygin@yandex.ru>
2018-04-23 23:17:10 +0300

    some fixes

--- application/controllers/controller_login.php
+++ application/controllers/controller_login.php
@@ -14,9 +14,9 @@ class Controller_Login extends Controller
             if (isset($user)) {
                 $data["login_status"] = "access_granted";
 
-                //session_start();
+                session_start();
                 $_SESSION['user'] = $user;
-                header('Location:/');
+                header('Location:/finances');
             } else {
                 $data["login_status"] = "access_denied";
             }
