dd4faacbb70b90451f3fbf853c26c066d9ba10a6
Sashkalol <fairprot@mail.ru>
2018-04-23 19:45:14 +0300

    Правки css от говна

--- application/views/login_view.php
+++ application/views/login_view.php
@@ -1,28 +1,28 @@
 <h1>Sign in page</h1>
 <p>
 <form action="" method="post">
-<table class="login">
-	<tr>
-		<th colspan="2">Sign in</th>
-	</tr>
-	<tr>
-		<td>login</td>
-		<td><input type="text" name="login" required></td>
-	</tr>
-	<tr>
-		<td>password</td>
-		<td><input type="password" name="password" required></td>
-	</tr>
-	<th colspan="2" style="text-align: center">
-	<input type="submit" value="Submit" name="btn"
-	style="width: 150px; height: 30px;"></th>
-</table>
+    <table class="login">
+        <tr>
+            <th colspan="2">Sign in</th>
+        </tr>
+        <tr>
+            <td>login</td>
+            <td><input type="text" name="login" required></td>
+        </tr>
+        <tr>
+            <td>password</td>
+            <td><input type="password" name="password" required></td>
+        </tr>
+        <th colspan="2" style="text-align: center">
+            <input type="submit" value="Submit" name="btn"
+                   style="width: 150px; height: 30px;"></th>
+    </table>
 </form>
 </p>
 
 <?php extract($data); ?>
-<?php if($login_status=="access_granted") { ?>
-<p style="color:green">Success.</p>
-<?php } elseif($login_status=="access_denied") { ?>
-<p style="color:red">Login and/or password was wrong.</p>
+<?php if ($login_status == "access_granted") { ?>
+    <p style="color:green">Success.</p>
+<?php } elseif ($login_status == "access_denied") { ?>
+    <p style="color:red">Login and/or password was wrong.</p>
 <?php } ?>
--- application/views/main_view.php
+++ application/views/main_view.php
@@ -1,6 +1,6 @@
-<h1 align= "center">Welcome to Home Finances App!</h1>
+<h1 align="center">Welcome to Home Finances App!</h1>
 <p>
-<!--  <img src="/images/...jpg" align="left">-->
-	    <? session_start() ?>
-  Hello, <? echo $_SESSION['user']->name; ?></p>
-  <h2><a href="/login" id="loginform">Sign in</a> | <a href="registration" id="registerform">Registration</a></h2>
+    <!--  <img src="/images/...jpg" align="left">-->
+    <? session_start() ?>
+    Hello, <? echo $_SESSION['user']->name; ?></p>
+<h2><a href="/login" id="loginform">Sign in</a> | <a href="registration" id="registerform">Registration</a></h2>
