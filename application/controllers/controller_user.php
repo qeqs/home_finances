9f81b61d73dc231ce36ae23db4ea34900f919c0f
qeqs <vadimlygin@yandex.ru>
2018-04-23 23:57:52 +0300

    some fixes

--- application/controllers/controller_user.php
+++ application/controllers/controller_user.php
@@ -4,7 +4,7 @@ class controller_user extends Controller
 {
     public static function isGuest()
     {
-        error_log("User " . $_SESSION['user'] . " logged: " . isset($_SESSION['user']));
+        //error_log("User " . $_SESSION['user'] . " logged: " . isset($_SESSION['user']));
         return !isset($_SESSION['user']);
     }
 }
